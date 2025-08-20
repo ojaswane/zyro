1e0774a223c62dc1bd94682f4e1e4b88a2a4000a
ojas wane <ojaswane79@gmail.com>
2025-08-20 10:12:29 +0530

    updated landing page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -11,12 +11,12 @@ export default function Home() {
           </div>
         </div>
 
-        <div >
+        <div className="flex flex-col items-center">
             <h1 className="text-black text-2xl font-bold text-center ">Discover People</h1>
             <p className="text-lg font-light text-gray-500 mt-10">Find entrepreneurs, investors, and innovators in your field</p>
 
 
-            <input type="search" name="" id=""  className="w-96 h-20 text-black border-2"/>
+            <input type="search" placeholder="Search " name="" id=""  className="w-96 p-3 mt-6 text-black border-3  border-gray-400 rounded focus:border-gray-400 "/>
         </div>
       </div>
     </>
