acf3f399de8f42a6609c2c1012cd6b1f8c08f373
ojas wane <ojaswane79@gmail.com>
2025-08-21 20:37:37 +0530

    updated landing page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -6,7 +6,7 @@ export default async function Home({ searchParams }: { searchParams: Promise<{ q
     <>
       <div className="flex items-center h-screen w-full flex-col bg-white ">
         <div className="my-20 font-bold  text-black text-6xl  w-1/2">
-          <h2 className="text-center">Turn Your Ideas Into <br /> Reality</h2>
+          <h2 className="text-center  font-lobster">Turn Your Ideas Into <br /> Reality</h2>
           <div className="text-gray-500 text-center text-xl mt-10 font-light">
             Connect with investors, showcase your vision, and build the future. Join thousands of entrepreneurs who are already making their mark.
           </div>
