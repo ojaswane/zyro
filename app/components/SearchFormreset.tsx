de1a89a9adb167c5b0262381c38e4c2e25681c45
ojas wane <ojaswane79@gmail.com>
2025-08-23 11:09:13 +0530

    added components

--- app/components/SearchFormreset.tsx
+++ app/components/SearchFormreset.tsx
@@ -1,6 +1,7 @@
 "use client"
 import React from 'react'
 import Link from 'next/link'
+import { X } from 'lucide-react'
 function SearchFormreset() {
 
     const reset = () => {
@@ -12,8 +13,8 @@ function SearchFormreset() {
     // This function resets the form when the button is clicked
   return (
     <button type="reset" onClick={reset}>
-        <Link href='/' className="flex items-center justify-center bg-black text-white rounded-full h-8 w-8 mx-1">
-            X
+        <Link href='/' className="flex mx-1">
+            <X className="size-5" />
         </Link>
     </button>
   )
--- app/components/Searchcomponent.tsx
+++ app/components/Searchcomponent.tsx
@@ -1,29 +1,26 @@
 import Form from 'next/form';
 import SearchFormreset from './SearchFormreset';
-
-function Searchcomponent( query: { query: string }) {
+import { Search } from 'lucide-react';
+function Searchcomponent(query: { query: string }) {
   return (
 
 
-    <Form action="/"  className="w-2xl  mt-8 text-black bg-gray-200 border-gray-400 rounded focus:outline-none focus:ring-2 focus:ring-gray-500">
-        <input 
-            name='query'
-            defaultValue= ""
-            placeholder='Search for people, ideas, or projects...'
-            className='w-full h-full p-3.5'
+    <Form action="/" className="w-2xl  mt-8 text-black bg-gray-200 border-gray-400 rounded focus:outline-none focus:ring-0 ">
+      <div className='flex '>
+        <input
+          name='query'
+          defaultValue=""
+          placeholder='Search for people, ideas, or projects...'
+          className=' w-full h-full p-3.5'
         />
 
-        <div className='flex gap-2 h-12 absolute w-96 right-20 top-170  justify-end items-center '> 
-                {query && <SearchFormreset />}
-
-            <button type="submit" className=" flex items-center justify-center bg-black text-white rounded-full h-8 w-8 mx-2 "> 
-                s
-            </button>
-        </div> 
+        {query && <SearchFormreset />}
+        <button type="submit" className='' >
+          <Search className='size-5 mx-3' />
+        </button>
+      </div>
     </Form>
   )
 }
 
 export default Searchcomponent
-
-//  className="w-2xl p-3.5 mt-8 text-black  bg-gray-200 border-gray-400 rounded focus:outline-none focus:ring-2 focus:ring-gray-500"
