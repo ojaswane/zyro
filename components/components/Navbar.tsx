3728752966c29b4831ba299ec3d6de83f21d0c83
ojas wane <ojaswane79@gmail.com>
2025-09-03 22:35:44 +0530

    App updated

--- components/components/Navbar.tsx
+++ components/components/Navbar.tsx
@@ -1,15 +1,15 @@
 // Navbar.tsx (Server Component)
 import React from "react";
 import Link from "next/link";
-import { auth } from "@/auth"; // ✅ server-side helper
-import Authbutton from "./Authbutton"; // 👈 new client component
+import { auth } from "@/auth"; 
+import Authbutton from "./Authbutton"; 
 
 const Navbar = async () => {
   const session = await auth();
 
   return (
-    <header className="fixed top-0 left-0 w-full z-50 px-5 py-3 font-work-sans bg-transparent backdrop-blur-sm text-white">
-      <nav className="flex justify-between items-center">
+    <header className="fixed top-0 left-0 w-full z-50 px-5 py-3  bg-transparent backdrop-blur-sm text-white">
+      <nav className="flex justify-between items-center px-20 ">
         <Link href="/" className="text-2xl font-bold font-monaco">
           Zyro
         </Link>
