114a0e68152eb875fae87207448ee78354e1d74b
ojas wane <ojaswane79@gmail.com>
2025-09-15 22:19:14 +0530

    debugging app

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,6 +1,6 @@
 import { type SchemaTypeDefinition } from 'sanity'
 import author from './author'
-import startup from './startups'
+import startup from './startup'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
   types: [
