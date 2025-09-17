8cbf883db5f01096d92d1c83190d297718fc2711
ojas wane <ojaswane79@gmail.com>
2025-09-17 23:23:29 +0530

    debugging

--- sanity/lib/quries.ts
+++ sanity/lib/quries.ts
@@ -1,26 +1,16 @@
-import { defineQuery } from "next-sanity";
-
-const STARTUP_QUERY = defineQuery(`
-*[_type == "startup" && defined(slug.current)] | order(_createdAt desc) {
+const STARTUP_QUERY = defineQuery(`*[_type == "startup"] | order(_createdAt desc){
   _id,
   title,
   _createdAt,
   slug,
-  views,
-  description,
-  category,
-  image {
-    asset->{url}
-  },
   author -> {
     _id,
     name,
-    bio,
-    image {
-      asset->{url}
-    }
-  }
-}
-`);
-
-export default STARTUP_QUERY;
+    image,
+    bio
+  },
+  views,
+  description,
+  category,
+  image    
+}`)
